a4e014e9e7266399b69cdef7f3279fa775e483a6
Esimtel-DEV <luis.leyton@esimtel.cl>
2025-02-20 17:20:52 -0300

    Update Playlist.jsx prettier

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FaHeart, FaEllipsisV  } from "react-icons/fa";
+import { FaHeart, FaEllipsisV } from "react-icons/fa";
 import cover1 from "../assets/images/cover1.webp";
 import cover2 from "../assets/images/cover2.webp";
 import cover3 from "../assets/images/cover3.webp";
@@ -11,18 +11,60 @@ function Playlist({ setCurrentTrack }) {
   const [likedSongs, setLikedSongs] = useState({});
 
   const songs = [
-    { id: 1, title: "Night Terror", artist: "Dream Theater", genre: "Progressive Metal", plays: "3.462.412", cover: cover1 },
-    { id: 2, title: "Nightmare", artist: "Avenged Sevenfold", genre: "Hard Rock", plays: "4.896.433", cover: cover2 },
-    { id: 3, title: "Windowpain", artist: "Opeth", genre: "Progressive Metal", plays: "6.354.676", cover: cover3 },
-    { id: 4, title: "Blackest Eyes", artist: "Porcupine Tree", genre: "Progressive Rock", plays: "3.544.790", cover: cover4 },
-    { id: 5, title: "What I've Done", artist: "Linkin Park", genre: "Nu Metal", plays: "8.975.234", cover: cover5 },
-    { id: 6, title: "My Curse", artist: "Killswitch Engage", genre: "Metal", plays: "3.799.251", cover: cover6 },
+    {
+      id: 1,
+      title: "Night Terror",
+      artist: "Dream Theater",
+      genre: "Progressive Metal",
+      plays: "3.462.412",
+      cover: cover1
+    },
+    {
+      id: 2,
+      title: "Nightmare",
+      artist: "Avenged Sevenfold",
+      genre: "Hard Rock",
+      plays: "4.896.433",
+      cover: cover2
+    },
+    {
+      id: 3,
+      title: "Windowpain",
+      artist: "Opeth",
+      genre: "Progressive Metal",
+      plays: "6.354.676",
+      cover: cover3
+    },
+    {
+      id: 4,
+      title: "Blackest Eyes",
+      artist: "Porcupine Tree",
+      genre: "Progressive Rock",
+      plays: "3.544.790",
+      cover: cover4
+    },
+    {
+      id: 5,
+      title: "What I've Done",
+      artist: "Linkin Park",
+      genre: "Nu Metal",
+      plays: "8.975.234",
+      cover: cover5
+    },
+    {
+      id: 6,
+      title: "My Curse",
+      artist: "Killswitch Engage",
+      genre: "Metal",
+      plays: "3.799.251",
+      cover: cover6
+    }
   ];
 
-  const toggleLike = (id) => {
-    setLikedSongs((prev) => ({
+  const toggleLike = id => {
+    setLikedSongs(prev => ({
       ...prev,
-      [id]: !prev[id],
+      [id]: !prev[id]
     }));
   };
 
@@ -30,33 +72,43 @@ function Playlist({ setCurrentTrack }) {
     <div className="playlist mt-3">
       <h4>Playlist</h4>
       <ul className="list-group">
-        {songs.map((song) => (
+        {songs.map(song =>
           <li key={song.id} className="list-group-item">
-            <div 
-              className="song-container" 
-              onClick={() => setCurrentTrack(song)} 
+            <div
+              className="song-container"
+              onClick={() => setCurrentTrack(song)}
               style={{ cursor: "pointer" }}
             >
               <div className="song-cover-wrapper">
-                <img src={song.cover} alt={`Cover de ${song.title}`} className="song-cover" />
+                <img
+                  src={song.cover}
+                  alt={`Cover de ${song.title}`}
+                  className="song-cover"
+                />
               </div>
               <div className="song-info">
-                <div className="song-title">{song.title}</div>
-                <div className="song-artist">{song.artist}</div>
+                <div className="song-title">
+                  {song.title}
+                </div>
+                <div className="song-artist">
+                  {song.artist}
+                </div>
               </div>
             </div>
-            <button 
-              className="like-button btn" 
-              onClick={(e) => {
+            <button
+              className="like-button btn"
+              onClick={e => {
                 e.stopPropagation();
                 toggleLike(song.id);
               }}
             >
               <FaHeart color={likedSongs[song.id] ? "red" : "gray"} />
             </button>
-            <button className="dots-button btn"><FaEllipsisV  color= "gray"/></button>
+            <button className="dots-button btn">
+              <FaEllipsisV color="gray" />
+            </button>
           </li>
-        ))}
+        )}
       </ul>
     </div>
   );
